7072ccee25f858ef293bcf5aaeabe36742092af0
meet7tank9 <meet7tank9@gmail.com>
2022-06-24 16:37:44 +0530

    array

--- array1.php
+++ array1.php
@@ -10,7 +10,14 @@
     <?php
         $cars=array("volvo","bmw","toyota");
         for($i=0; $i<count($cars); $i++){
-        echo "<br>".$cars[$i];
+            echo "<br>".$cars[$i];
+        }
+
+
+
+        $age=array("peter"=>"35", "ben"=>"37","joe"=>"79");
+        foreach ($age as $key => $value) {
+            echo "<br> $key is $value years old";
         }
     ?>
 </body>
